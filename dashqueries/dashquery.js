d7cff7040a4703bff0d563c46c2034e316361c54
Your Name <you@example.com>
2023-03-13 11:11:17 +0200

    Updating my bland with css on different pages

--- dashqueries/dashquery.js
+++ dashqueries/dashquery.js
@@ -12,7 +12,20 @@ const postContact = async (e) =>{
         body: JSON.stringify(doc),
         headers: {"Content-Type" : "application/json"}
     });
-    alert("your Message Sent successfully!!");
+     Toastify({
+            text: "Your Message sent Successfully",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: false,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #333, #444)",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
     
 }
 FormAdd && FormAdd.addEventListener("submit",postContact);
